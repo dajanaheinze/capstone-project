6c1ef5aeae6dce92433b19ff2badb3dc8dbf5256
Dajana Heinze <heinzedajana@gmail.com>
2020-11-17 15:49:57 +0100

    Deleted Styleguides Folder,  JSON Script,  uninstall Styleguides

--- styleguide.config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    ignore: ['**/App.js'],
-    defaultExample: true,
-    exampleMode: 'expand',
-    usageMode: 'expand',
-    components: 'src/components/**/[A-Z]*.js',
-  }
