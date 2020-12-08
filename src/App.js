23d760985fa53a2df083b6ff76511bd8f6bb34bc
Dajana Heinze <heinzedajana@gmail.com>
2020-12-08 09:25:12 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { Switch, Route } from 'react-router-dom'
+//import { Switch, Route } from 'react-router-dom'
 import styled from 'styled-components/macro'
 import Header from './components/AppHeader'
 import TutorialList from './components/TutorialList'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <AppLayout>
       <Header headline="Mind Reset" />
       <TutorialList />
+      <Footer />
     </AppLayout>
   )
 }
 
 const AppLayout = styled.div`
   display: grid;
-  grid-template-rows: 100px auto;
+  grid-template-rows: 100px auto 60px;
   position: relative;
   height: 100vh;
   margin: 0;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,2 +1,17 @@
 import styled from 'styled-components/macro'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
+import { ReactComponent as home } from '../images/home.svg'
+import { ReactComponent as about } from '../images/about.svg'
+
+export default function Footer() {
+  return (
+    <div>
+      <div>
+        <img src={home} alt="" />
+      </div>
+      <div>
+        <img src={about} alt="" />
+      </div>
+    </div>
+  )
+}
